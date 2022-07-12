6da4f23332f8c84d77e82aabd5f283b0339b8d18
hiroro-work <hiroro@sonicgarden.jp>
2022-07-12 10:04:23 +0900

    vitestでテストできるように

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,7 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
-})
+  test: {
+    environment: 'jsdom',
+  },
+});
