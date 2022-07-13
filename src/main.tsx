5dff8238992313fabdf929d3f70a29c616dda282
t-kojima <t-kojima@sonicgarden.jp>
2022-07-13 15:35:30 +0900

    WindiCSSの導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
+import 'virtual:windi.css';
 import '@/lib/firebase';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
