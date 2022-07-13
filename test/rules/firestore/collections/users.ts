08e5cbc844545f60de96654f17b1afd347761810
hiroro-work <hiroro@sonicgarden.jp>
2022-07-13 12:03:56 +0900

    usersコレクションのruleテスト作成

--- test/rules/firestore/collections/users.ts
+++ test/rules/firestore/collections/users.ts
@@ -52,4 +52,71 @@ export const usersTest = () => {
       await assertFails(ref.delete());
     });
   });
+
+  describe('認証済の場合', () => {
+    it('一覧を読み込みできる(list)', async () => {
+      const db = env.authenticatedContext(user.id).firestore();
+      const ref = db.collection('users');
+      await assertSucceeds(ref.get());
+    });
+
+    describe('自分のデータの場合', () => {
+      let db: firebase.firestore.Firestore;
+
+      beforeEach(() => {
+        db = env.authenticatedContext(user.id).firestore();
+      });
+
+      it('読み込みできる(get)', async () => {
+        const ref = db.collection('users').doc(user.id);
+        await assertSucceeds(ref.get());
+      });
+
+      it('作成できる', async () => {
+        const newUser = userFactory.build();
+        const db = env.authenticatedContext(newUser.id).firestore();
+        const ref = db.collection('users');
+        await assertSucceeds(ref.doc(newUser.id).set(newUser));
+      });
+
+      it('更新できる', async () => {
+        const ref = db.collection('users').doc(user.id);
+        await assertSucceeds(ref.update({ name: '違う名前' }));
+      });
+
+      it('削除できる', async () => {
+        const ref = db.collection('users').doc(user.id);
+        await assertSucceeds(ref.delete());
+      });
+    });
+
+    describe('自分以外のデータの場合', () => {
+      let db: firebase.firestore.Firestore;
+
+      beforeEach(() => {
+        db = env.authenticatedContext(user.id).firestore();
+      });
+
+      it('読み込みできる(get)', async () => {
+        const ref = db.collection('users').doc(other.id);
+        await assertSucceeds(ref.get());
+      });
+
+      it('作成できない', async () => {
+        const newUser = userFactory.build();
+        const ref = db.collection('users');
+        await assertFails(ref.doc(newUser.id).set(newUser));
+      });
+
+      it('更新できない', async () => {
+        const ref = db.collection('users').doc(other.id);
+        await assertFails(ref.update({ name: '違う名前' }));
+      });
+
+      it('削除できない', async () => {
+        const ref = db.collection('users').doc(other.id);
+        await assertFails(ref.delete());
+      });
+    });
+  });
 };
