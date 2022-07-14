4f6914f6ee7a4e8f4aa29f887774649e98cfda6d
t-kojima <t-kojima@sonicgarden.jp>
2022-07-14 22:43:52 +0900

    UsersContext.testの追加

--- src/contexts/UsersContext.tsx
+++ src/contexts/UsersContext.tsx
@@ -1,25 +1,24 @@
 import { ReactNode, createContext, useContext, useMemo } from 'react';
-import { LoadingScreen } from '@/components/LoadingScreen';
 import { UserWithId, User } from '@/types/user';
 import { useCollectionData } from '@/hooks/useCollectionData';
 import { keyBy } from 'lodash-es';
 
 type UsersContextValue = {
-  users: UserWithId[] | null;
+  users: UserWithId[];
   usersById: { [id: string]: UserWithId };
+  loading: boolean;
 };
 
-export const UsersContext = createContext<UsersContextValue>({ users: null, usersById: {} });
+export const UsersContext = createContext<UsersContextValue>({ users: [], usersById: {}, loading: true });
 
 export const UsersProvider = ({ children }: { children: ReactNode }) => {
   const [users, loading] = useCollectionData<User>('users');
-  if (loading || !users) return <LoadingScreen />;
   const usersById = useMemo(() => keyBy(users, 'id'), [users]);
 
-  return <UsersContext.Provider value={{ users, usersById }}>{children}</UsersContext.Provider>;
+  return <UsersContext.Provider value={{ users: users || [], usersById, loading }}>{children}</UsersContext.Provider>;
 };
 
 export const useUsers = () => {
-  const { users, usersById } = useContext(UsersContext);
-  return { users, usersById };
+  const { users, usersById, loading } = useContext(UsersContext);
+  return { users, usersById, loading };
 };
