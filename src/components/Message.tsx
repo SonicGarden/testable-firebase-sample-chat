58de6a22a8b3752719993ca61f0a5261a5faf703
t-kojima <t-kojima@sonicgarden.jp>
2022-07-14 23:16:06 +0900

    Messageコンポーネントのpropsからsenderを排除

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,13 +1,16 @@
+import { useUsers } from '@/contexts/UsersContext';
 import { MessageWithId } from '@/types/message';
-import { UserWithId } from '@/types/user';
 import { format } from 'date-fns';
 
-export const Message = ({ message, sender }: { message: MessageWithId; sender: UserWithId }) => {
+export const Message = ({ message }: { message: MessageWithId }) => {
+  const { usersById, loading } = useUsers();
+  const sender = usersById[message.senderId];
+  if (loading) return <div>loading...</div>;
   return (
     <div>
       <div>
-        <img src={sender.photoUrl} />
-        <span>{sender.name}</span>
+        <img src={sender?.photoUrl} />
+        <span>{sender?.name || '名無しさん'}</span>
         <span>{format(message.createdAt.toDate(), 'yyyy-MM-dd HH:mm')}</span>
       </div>
       <p>{message.content}</p>
--- test/components/Message.test.tsx
+++ test/components/Message.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it, expect, afterEach } from 'vitest';
-import { render, cleanup, screen } from '@testing-library/react';
+import { describe, it, expect, afterEach, vi } from 'vitest';
+import { render, cleanup, screen, waitFor } from '@testing-library/react';
 import { Message } from '@/components/Message';
 import { userFactory } from '@/../test/factories/user';
 import { messageFactory } from '@/../test/factories/message';
@@ -7,6 +7,12 @@ import { Timestamp } from 'firebase/firestore';
 
 describe('Message', () => {
   afterEach(() => cleanup());
+
+  vi.mock('@/context/UsersContext', () => {
+    return {
+      useUsers: { usersById: { 'user-id': [sender] } },
+    };
+  });
 
   const sender = userFactory.build({
     id: 'user-id',
@@ -20,23 +26,28 @@ describe('Message', () => {
     createdAt: Timestamp.fromDate(new Date('2022-07-01 00:00:00+09:00')),
   });
 
+  it('loading中はloadingメッセージが表示される', () => {
+    render(<Message message={message} />);
+    expect(screen.getByText('loading...')).toBeTruthy();
+  })
+
   it('アイコン画像が表示される', () => {
-    render(<Message message={message} sender={sender} />);
-    expect(screen.getByRole('img').getAttribute('src')).toBe('dummy-url');
+    render(<Message message={message} />);
+    waitFor(() => expect(screen.getByRole('img').getAttribute('src')).toBe('dummy-url'));
   });
 
   it('送信者の名前が表示される', () => {
-    render(<Message message={message} sender={sender} />);
-    expect(screen.getByText('テストユーザー')).toBeTruthy();
+    render(<Message message={message} />);
+    waitFor(() => expect(screen.getByText('テストユーザー')).toBeTruthy());
   });
 
   it('送信時間が表示される', () => {
-    render(<Message message={message} sender={sender} />);
-    expect(screen.getByText('2022-07-01 00:00')).toBeTruthy();
+    render(<Message message={message} />);
+    waitFor(() => expect(screen.getByText('2022-07-01 00:00')).toBeTruthy());
   });
 
   it('メッセージが表示される', () => {
-    render(<Message message={message} sender={sender} />);
-    expect(screen.getByText('テストのメッセージ')).toBeTruthy();
+    render(<Message message={message} />);
+    waitFor(() => expect(screen.getByText('テストのメッセージ')).toBeTruthy());
   });
 });
