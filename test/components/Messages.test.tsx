8756fd4531bf6ccc60f84d86de15cb7e6eb78b45
hiroro-work <hiroro@sonicgarden.jp>
2022-07-19 16:02:03 +0900

    fix a bit

--- test/components/Messages.test.tsx
+++ test/components/Messages.test.tsx
@@ -1,5 +1,6 @@
 import { render, cleanup, screen } from '@testing-library/react';
 import { Messages } from '@/components/Messages';
+import * as useCollectionData from '@/hooks/useCollectionData';
 
 describe('Messages', () => {
   afterEach(() => {
@@ -8,30 +9,19 @@ describe('Messages', () => {
   });
 
   it('ローディング中の場合、ローディング画面が表示される', () => {
-    vi.mock('@/hooks/useCollectionData', () => {
-      return {
-        useCollectionData: () => [[], true],
-      };
-    });
-
+    vi.spyOn(useCollectionData, 'useCollectionData').mockReturnValue([[], true, undefined, undefined]);
     render(<Messages />);
     expect(screen.getByText('loading...')).toBeTruthy();
   });
 
-  it('ローディング完了後、メッセージ一覧が表示される', () => {
-    vi.mock('@/hooks/useCollectionData', async () => {
-      const { messageFactory } = await import('@/../test/factories/message');
-      const message = messageFactory.build({
-        id: 'test-message-id',
-        content: 'テストメッセージ',
-        senderId: 'test-user-uid',
-      });
-
-      return {
-        useCollectionData: () => [[message], false],
-      };
+  it('ローディング完了後、メッセージ一覧が表示される', async () => {
+    const { messageFactory } = await import('@/../test/factories/message');
+    const message = messageFactory.build({
+      id: 'test-message-id',
+      content: 'テストメッセージ',
+      senderId: 'test-user-uid',
     });
-
+    vi.spyOn(useCollectionData, 'useCollectionData').mockReturnValue([[message], false, undefined, undefined]);
     render(<Messages />);
     expect(screen.getByText('Messages')).toBeTruthy();
   });
