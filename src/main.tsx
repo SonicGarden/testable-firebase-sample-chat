b172b68ad27d0eba9a3f07a4ba7a485f99e8dc3d
t-kojima <t-kojima@sonicgarden.jp>
2022-07-21 21:53:12 +0900

    画面が動くように

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import 'virtual:windi.css';
 import '@/lib/firebase';
+import { AuthProvider } from './contexts/AuthContext';
+import { UsersProvider } from './contexts/UsersContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <UsersProvider>
+        <App />
+      </UsersProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
