260b4c098625eb612555bd0074e87a6daad0776a
hiroro-work <hiroro@sonicgarden.jp>
2022-08-06 22:07:31 +0900

    fix a bit

--- functions/vitest.config.ts
+++ functions/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: 'node',
     include: ['./test/**/*.test.{js,jsx,ts,tsx}'],
   },
