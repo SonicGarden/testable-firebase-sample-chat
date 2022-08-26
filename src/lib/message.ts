0f19ae0596932fba18786ebc7d28d1a1e4186c49
t-kojima <t-kojima@sonicgarden.jp>
2022-08-26 21:05:11 +0900

    fix: messageクエリを変数から関数に

--- src/lib/message.ts
+++ src/lib/message.ts
@@ -2,6 +2,6 @@ import { getFirestore, collection, query, orderBy } from 'firebase/firestore';
 import { getConverter } from '@/lib/firebase';
 import type { MessageDocumentData } from '@/types/message';
 
-export const messagesRef = collection(getFirestore(), 'messages').withConverter(getConverter<MessageDocumentData>());
+export const messagesRef = () => collection(getFirestore(), 'messages').withConverter(getConverter<MessageDocumentData>());
 
-export const messagesQuery = query(messagesRef, orderBy('createdAt', 'asc'));
+export const messagesQuery = () => query(messagesRef(), orderBy('createdAt', 'asc'));
