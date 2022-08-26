ed17f0c30047668fe08fe0e371afe681e4b5d238
t-kojima <t-kojima@sonicgarden.jp>
2022-08-26 21:58:59 +0900

    fix: userSecretsRef変数を関数に

--- src/lib/userSecret.ts
+++ src/lib/userSecret.ts
@@ -2,12 +2,12 @@ import { getFirestore, collection, doc, getDoc, setDoc } from 'firebase/firestor
 import { getConverter, serverTimestamp } from '@/lib/firebase';
 import type { UserSecretDocumentData } from '@/shared/types/userSecret';
 
-export const userSecretsRef = collection(getFirestore(), 'userSecrets').withConverter(
+export const userSecretsRef = () => collection(getFirestore(), 'userSecrets').withConverter(
   getConverter<UserSecretDocumentData>()
 );
 
 export const getUserSecret = async (uid: string) => {
-  const snapshot = await getDoc(doc(userSecretsRef, uid));
+  const snapshot = await getDoc(doc(userSecretsRef(), uid));
   const isExist = snapshot.exists();
   const userSecret = snapshot.data();
 
@@ -16,5 +16,5 @@ export const getUserSecret = async (uid: string) => {
 
 export const setUserSecret = async (uid: string, { fcmToken }: { fcmToken: string }) => {
   const userSecret = { fcmToken, createdAt: serverTimestamp() };
-  await setDoc(doc(userSecretsRef, uid), userSecret, { merge: true });
+  await setDoc(doc(userSecretsRef(), uid), userSecret, { merge: true });
 };
