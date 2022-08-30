b504f095b47618b620b53b0125cb2aac747de050
t-kojima <t-kojima@sonicgarden.jp>
2022-08-30 13:28:14 +0900

    feat: メッセージ表示test追加

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -6,16 +6,28 @@ const user = {
   emailVerified: true,
 };
 
+const message = {
+  content: 'test message',
+  imagePath: null,
+  senderId: 'DUMMY-USER-ID',
+}
+
 describe('画面が表示されること', () => {
   beforeEach(() => {
     cy.task('create:user', user);
+    cy.task('create:message', message);
     cy.login(user.uid);
   })
   afterEach(() => {
     cy.teardown();
   });
-  it('passes', () => {
+  it('タイトルが表示される', () => {
     cy.visit('/')
     cy.contains('Sample Chat App');
+  })
+
+  it('メッセージが表示される', () => {
+    cy.visit('/')
+    cy.contains('test message')
   })
 })
